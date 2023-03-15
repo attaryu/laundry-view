c07f388a781d4a48da8d7c1d3007d3b4c760d9fb
attaryu <mattarannaufal@gmail.com>
2023-03-15 09:16:20 +0700

    404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,14 @@
 export default function PageNotFound() {
   return (
-    <div className="w-full">
-      <h1>404 Not Found</h1>
-    </div>
+    <main className="w-full h-screen grid place-items-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-zinc-700">
+          404
+        </h1>
+        <small className="font-semibold text-zinc-400">
+          Page Not Found
+        </small>
+      </div>
+    </main>
   );
 }
